8144369a6129116aa58b342cc740131056ebf4ae
StanislavK <542557+StanislavK@users.noreply.github.com>
2024-01-20 18:55:07 +0100

    Initial Commit

---
+++ DrivesMeCrazy/DrivesMeCrazyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrivesMeCrazyApp.swift
+//  DrivesMeCrazy
+//
+//  Created by Stanislav Kasprik on 20.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct DrivesMeCrazyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
